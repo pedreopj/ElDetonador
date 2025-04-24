b7971c5cb468f31d5601b8f19352457e3095313d
pedreopj <pelaezescobarpepo@gmail.com>
2025-04-24 18:44:51 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-INFLUX_URL = "your_influx_db_url"
-INFLUX_TOKEN = "your_influx_db_token"
-ORG = "organization_name"
-BUCKET = "bucket_name"
+INFLUX_URL = " "https://us-east-1-1.aws.cloud2.influxdata.com"
+INFLUX_TOKEN =  "rnRx-Nk8dXeumEsQeDT4hk78QFWNTOVim7UrH5fnYKVSoQQIkhCwKq03-UMKN-S0Nj-DbfmrMD0HUI61qRJaiw=="
+ORG = ""0925ccf91ab36478"
+BUCKET = ""homeiot"
