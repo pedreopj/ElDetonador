e6ba7d7d00c6bf4f0529a073e8fd392cec53220a
pedreopj <pelaezescobarpepo@gmail.com>
2025-04-24 19:25:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def query_gyroscope_data(range_minutes=60):
     import "math"
     from(bucket: "{BUCKET}")
       |> range(start: -{range_minutes}m)
-      |> filter(fn: (r) => r["_measurement"] == "gyro_magnitude" and r["_field"] == "gx" or r["_field"] == "gy" or r["_field"] == "gz")
+      |> filter(fn: (r) => r["_measurement"] == "accelerometer" and r["_field"] == "gx" or r["_field"] == "gy" or r["_field"] == "gz")
       |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
       |> sort(columns: ["_time"])
     '''
